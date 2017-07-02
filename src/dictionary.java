429da76531a8ff388fe37ac318ee80de0c09c927
Shuai-Hsun <victory2152@livemail.tw>
2017-07-01 18:57:32 -0700

    dictionary memory usage optimize

--- src/dictionary.java
+++ src/dictionary.java
@@ -4,7 +4,9 @@ import java.util.LinkedList;
 public class dictionary extends node
 {
 	private node root;
+
 	dictionary() { root = new node(); }
+
 	public void insertWord(String str)
 	{
 		node tmp = root;
@@ -18,17 +20,19 @@ public class dictionary extends node
 		if (tmp.getStatus() == false)
 			tmp.modifyStatus();
 	}
+
 	public boolean isWord(String str)
 	{
 		node tmp = root;
+		char ch;
 		for (int i=0; i<str.length(); i++)
 		{
-			char ch = str.charAt(i);
-			if (!tmp.existChild(ch))
-				return false;
-			// Upper case and lower case are same
+			ch = str.charAt(i);
+			// Turn Upper case into lower case 
 			if (65<=ch && ch<=90)
 				ch += 32;
+			if (!tmp.existChild(ch))
+				return false;
 			tmp = tmp.nextChild(tmp, ch);
 			if (tmp.getChildren() == null)
 				return false;
@@ -37,46 +41,56 @@ public class dictionary extends node
 	}
 }
 
+// efficient usage of memory
 class node
 {
-	private char key;
+	private boolean[] key;
 	private boolean status = false;
 	private LinkedList<node> children = null;
+
 	public node() {}
 	public node(char ch)
 	{
-		key = ch;
+		key = new boolean[5];
+		// convert alphabet to 5 bit
+		for (int i=0; i<5; i++)
+			key[i] = ((((int)ch - 97) >> i) & 1) == 1 ? true : false;
 	}
-	protected char getKey()
+
+	public boolean[] chToBool(char ch)
 	{
-		return key;
+		boolean[] bit = new boolean[5]; 
+		for (int i=0; i<5; i++)
+			bit[i] = ((((int)ch - 97) >> i) & 1) == 1 ? true : false;
+		return bit;
 	}
-	protected boolean getStatus()
+
+	protected boolean[] getKey() { return key; }
+
+	protected boolean getStatus() { return status; }
+
+	protected LinkedList<node> getChildren() { return children; }
+
+	protected void modifyStatus() { status = !status; }
+
+	private boolean isEqual(boolean[] bit1, boolean[] bit2)
 	{
-		return status;
+		for (int i=0; i<bit1.length; i++)
+			if (bit1[i] != bit2[i])
+				return false;
+		return true;
 	}
-	protected LinkedList<node> getChildren()
-	{
-		return children;
-	}
-	protected void modifyStatus()
-	{
-		status = !status;
-	}
-	protected char upperCase(char ch)
-	{
-		if (97<=ch && ch<=122)
-			ch -= 32;
-		return ch;
-	}
+
 	protected boolean existChild(char ch)
 	{
+		boolean[] bits = chToBool(ch);
 		// upper case and lower case are same
-		for (int i=0; i<children.size(); i++)
-			if (children.get(i).key == ch || upperCase(children.get(i).key) == ch)
+		for (node n : children)
+			if (isEqual(n.key, bits))
 				return true;
 		return false;
 	}
+
 	protected void addChild(char ch)
 	{
 		if (children == null)
@@ -88,11 +102,60 @@ class node
 			if (!existChild(ch))
 				children.add(new node(ch));
 	}
+
 	protected node nextChild(node obj, char ch)
 	{
-		for (int i=0; i<obj.children.size(); i++)
-			if (obj.children.get(i).key == ch)
-				return obj.children.get(i);
+		boolean[] bits = chToBool(ch);
+		for (node n : obj.children)
+			if (isEqual(n.key, bits))
+				return n;
 		return null;
 	}
 }
+
+// class node
+// {
+// 	private char key;
+// 	private boolean status = false;
+// 	private LinkedList<node> children = null;
+
+// 	public node() {}
+
+// 	public node(char ch) { key = ch; }
+
+// 	protected char getKey() { return key; }
+
+// 	protected boolean getStatus() { return status; }
+
+// 	protected LinkedList<node> getChildren() { return children; }
+
+// 	protected void modifyStatus() { status = !status; }
+
+// 	protected boolean existChild(char ch)
+// 	{
+// 		for (node n : children)
+// 			if (n.key == ch)
+// 				return true;
+// 		return false;
+// 	}
+
+// 	protected void addChild(char ch)
+// 	{
+// 		if (children == null)
+// 		{
+// 			children = new LinkedList<node>();
+// 			children.add(new node(ch));
+// 		}
+// 		else
+// 			if (!existChild(ch))
+// 				children.add(new node(ch));
+// 	}
+
+// 	protected node nextChild(node obj, char ch)
+// 	{
+// 		for (node n : obj.children)
+// 			if (n.key == ch)
+// 				return n;
+// 		return null;
+// 	}
+// }
